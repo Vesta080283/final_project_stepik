db72f3e38e053de65260e2ae2c66f13c86259cef
Vesta080283 <43104215+Vesta080283@users.noreply.github.com>
2021-02-03 20:17:48 +0600

    add new files
    add folder 'pages' with files: base_page.py, main_page.py, __init__.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,23 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+from pages.main_page import MainPage
+
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def test_guest_can_go_to_login_page(browser):
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)
+    page.open()                      # открываем страницу
+    # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
